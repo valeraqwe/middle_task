7072a9c549f8faf62041537be34e63f66daa6277
Valeriy <valera.komar.1999@gmail.com>
2024-08-15 19:12:14 +0300

    Adding Caching to Popular Products Query

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\Product;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 
 class ProductService
 {
@@ -14,5 +15,15 @@ class ProductService
         return Product::whereHas('purchases', function($query) use ($thirtyDaysAgo) {
             $query->where('created_at', '>=', $thirtyDaysAgo);
         })->get();
+    }
+
+    public function getPopularProducts()
+    {
+        return Cache::remember('popular_products', 60 * 24 * 30, function () {
+            return Product::where('purchases_count', '>', 0)
+                ->orderBy('purchases_count', 'desc')
+                ->take(10)
+                ->get();
+        });
     }
 }
