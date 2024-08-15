90d99c93df4a4feaa167f15b443624be0c4f86c6
Valeriy <valera.komar.1999@gmail.com>
2024-08-15 19:00:32 +0300

    customer, product, purchase models and migrations

---
+++ app/Models/Purchase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Purchase extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['customer_id', 'product_id', 'quantity', 'purchase_date'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+}
